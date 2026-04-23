64f9d5c4c4d25128f38de05aacbc222d07392551
vtrafimu <viktar.trafimuk@capgemini.com>
2026-04-23 12:46:28 +0200

    add Ex

---
+++ src/test/java/org/viktar/sci/ExampleTest.java
@@ -0,0 +1,10 @@
+package org.viktar.sci;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void exampleTest() {
+        System.out.println("This is an example test and it must pass.");
+    }
+}
